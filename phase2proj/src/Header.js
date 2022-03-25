6fcbcc9caab73627e81efb9fffe1692e4c6e6490
Sam <samuel.waters@gmail.com>
2022-03-25 12:55:46 -0600

    react app built out

---
+++ phase2proj/src/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+    return (
+        <reactfragment>
+            <div className="Header">
+                <h1>Choose your Cocktail!</h1>
+            </div>
+            <div class="content">
+            </div>
+        </reactfragment>
+    
+    );
+}
+  
+export default Header;
+  
---
+++ phase2proj/src/ProductList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ProductList.css';
+
+function ProductList() {
+    return (
+      <div className="ProductList">
+          <p>Main Space</p>
+      </div>
+    );
+}
+  
+export default ProductList;
+  
