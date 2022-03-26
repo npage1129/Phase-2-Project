38b29f10ca711f38e2105ac88e858dc966d933db
Sam <samuel.waters@gmail.com>
2022-03-26 07:14:04 -0600

    more basics

---
+++ phase2proj/src/SideBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './SideBar.css';
+
+function SideBar() {
+    return (
+        <div className="sidebar">
+            <p>inside side bar</p>
+        </div>
+    );
+}
+  
+export default SideBar;
+  
