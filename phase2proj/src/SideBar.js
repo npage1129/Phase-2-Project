863b4db7669634b5c8d75084387f3a374d48da9d
Sam <samuel.waters@gmail.com>
2022-03-28 17:38:36 -0600

    end of day 3.28

--- phase2proj/src/SideBar.js
+++ phase2proj/src/SideBar.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './SideBar.css';
 
+const api = "www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
+
 function SideBar() {
+    function handleData(data) {
+        console.log(data);
+    }
+
+const cocktail = 
+fetch('www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+    .then(response => response.json())
+    .then(data => console.log(data));
+
+
     return (
         <div className="sidebar">
-            <p>inside side bar</p>
+            <h3>Need Ideas? Try this random cocktail!</h3>
+            <p>cocktail</p>
         </div>
     );
 }
-  
+ 
 export default SideBar;
-  
