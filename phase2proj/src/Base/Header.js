804fe655960a2591f17e8803c3fbcc2fb6431603
Sam <samuel.waters@gmail.com>
2022-03-28 15:19:02 -0600

    details

--- phase2proj/src/Base/Header.js
+++ phase2proj/src/Base/Header.js
@@ -4,12 +4,12 @@ import './Header.css';
 function Header() {
     return (
         <div className="Header">
-            <nav ClassName="NavBar">
-                <a href="./Home">Home</a>
-                <a href="./About">About</a>
-                <a href="./Contact">Contact</a>
+            <nav ClassName="NavBar"> 
+                <a href="./Home" >Home<br></br></a> 
+                <a href="./About">About<br></br></a>
+                <a href="./Contact">Contact<br></br></a>
             </nav>
-            <h1>Header</h1>
+            <h1></h1>
         </div>
     );
 }
