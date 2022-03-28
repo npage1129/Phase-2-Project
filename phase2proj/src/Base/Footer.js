8155ada0ed8bd6627f737c1f9fefa0c7965156a5
Sam <samuel.waters@gmail.com>
2022-03-28 12:25:55 -0600

    more stuff

--- phase2proj/src/Base/Footer.js
+++ phase2proj/src/Base/Footer.js
@@ -5,20 +5,20 @@ import './Footer.css';
 function Footer() {
     return (
       <div className="Footer">
-        {/* <div className="links">
-          <p>Sam's Links</p>
+        <div className="links">
+          <p>Sam Waters</p>
+          <a href="https://www.facebook.com/" class="fa fa-instagram"></a>
+          <a href="https://www.facebook.com/" class="fa fa-twitter"></a>
+          <a href="https://www.facebook.com/" class="fa fa-facebook"></a>
+          <a href="https://www.facebook.com/" class="fa fa-github"></a>
+          <a href="#" class="fa fa-linkedin"></a>
+          {/* <p>Nicoles's Links</p>
             <a href="https://www.facebook.com/" class="fa fa-instagram"></a><span>Instagram</span>
             <a href="https://www.facebook.com/" class="fa fa-twitter"></a><span>Twitter</span>
             <a href="https://www.facebook.com/" class="fa fa-facebook"></a><span>Facebook</span>
             <a href="https://www.facebook.com/" class="fa fa-github"></a><span>GitHub</span>
-            <a href="#" class="fa fa-linkedin"></a><span>LinkedIn</span>
-          <p>Nicoles's Links</p>
-            <a href="https://www.facebook.com/" class="fa fa-instagram"></a><span>Instagram</span>
-            <a href="https://www.facebook.com/" class="fa fa-twitter"></a><span>Twitter</span>
-            <a href="https://www.facebook.com/" class="fa fa-facebook"></a><span>Facebook</span>
-            <a href="https://www.facebook.com/" class="fa fa-github"></a><span>GitHub</span>
-            <a href="https://www.facebook.com/" class="fa fa-linkedin"></a> <span>LinkedIn</span> 
-        </div>  */}
+            <a href="https://www.facebook.com/" class="fa fa-linkedin"></a> <span>LinkedIn</span>  */}
+        </div> 
       </div>
     );
 }
