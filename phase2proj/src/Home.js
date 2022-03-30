885af31acb609e809edac4e78be884131e58c583
npage1129 <82468860+npage1129@users.noreply.github.com>
2022-03-29 18:21:50 -0600

    ad
    asdf

--- phase2proj/src/Home.js
+++ phase2proj/src/Home.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import React,{useEffect,useState} from 'react';
-=======
 import React, {useEffect, useState} from 'react';
->>>>>>> main
 import Footer from './Base/Footer';
 import Header from './Base/Header.js';
 import SideBar from './SideBar';
@@ -10,16 +6,6 @@ import ProductList from './ProductList';
 import './Home.css';
 
 function Home() {
-<<<<<<< HEAD
-const  [get, setGet] = useState({});
-useEffect(()=> {
-fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
-  .then((resp) => resp.json())
-  .then((data) => setGet(data))
-},[])
-=======
-
->>>>>>> main
   return (
   
     <div className="Home">
@@ -30,11 +16,7 @@ fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
       <div>
 
       </div>
-<<<<<<< HEAD
-      <SideBar get ={get}/>
-=======
       <SideBar  />
->>>>>>> main
       <ProductList />
       <Footer />
     
--- phase2proj/src/ProductCard.js
+++ phase2proj/src/ProductCard.js
@@ -1,21 +1,22 @@
-import React from 'react';
+import React,{useState,UseEffect} from 'react';
 import './ProductCard.css';
 
 function ProductCard() {
+
     return (
-      <div className="ProductCard">
-          <img src="https://previews.123rf.com/images/sundaribardia/sundaribardia1604/sundaribardia160400052/56832824-familia-de-hippopotamidae-especie-amphibius-hipop%C3%B3tamo-agua-potable-en-la-orilla-del-r%C3%ADo.jpg" width="100px" height="100px"/>
-          <h1 ClassName="title">Cocktail Title</h1>
-          <ul>
-              <li ClassName="ingredient">Ingredient 1</li>
-              <li ClassName="ingredient">Ingredient 2</li>
-              <li ClassName="ingredient">Ingredient 3</li>
-              <li ClassName="ingredient">Ingredient 4</li>
-              <li ClassName="ingredient">Ingredient 5</li>
-          </ul>
-      </div>
+    <div className="ProductCard">
+        <img src="https://previews.123rf.com/images/sundaribardia/sundaribardia1604/sundaribardia160400052/56832824-familia-de-hippopotamidae-especie-amphibius-hipop%C3%B3tamo-agua-potable-en-la-orilla-del-r%C3%ADo.jpg" width="100px" height="100px"/>
+        <h1 ClassName="title">Cocktail Title</h1>
+        <ul>
+            <li ClassName="ingredient">Ingredient 1</li>
+            <li ClassName="ingredient">Ingredient 2</li>
+            <li ClassName="ingredient">Ingredient 3</li>
+            <li ClassName="ingredient">Ingredient 4</li>
+            <li ClassName="ingredient">Ingredient 5</li>
+        </ul>
+    </div>
     );
 }
-  
+
 export default ProductCard;
-  
+ 
